79a87b9486461143b97e87acb2a33bc58f625632
christopherayork <christopherayork1992@gmail.com>
2019-06-13 15:49:04 -0400

    Finished MVP & Stretch

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,159 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(person) {
+        this.name = person.name;
+        this.role = person.role;
+        this.location = person.location;
+        this.age = person.age;
+
+    }
+
+    speak() {
+        console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(person) {
+        super(person);
+        this.favLanguage = person.favLanguage;
+        this.specialty = person.specialty;
+        this.catchPhrase = person.catchPhrase;
+
+    }
+
+    static demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+
+    static grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}`);
+    }
+
+    modifyGrade(student) {
+        let modifyAmount = Math.floor(Math.random() * 100);
+        let possible = [1,2];
+        let operation = Math.floor(Math.random() * possible.length);
+        if(operation) student.grade += modifyAmount;
+        else student.grade -= modifyAmount;
+    }
+}
+
+class Student extends Person {
+    constructor(person) {
+        super(person);
+        this.previousBackground = person.previousBackground;
+        this.className = person.className;
+        this.favSubjects = person.favSubjects; // array
+        this.grade = Math.floor(Math.random() * 100);
+
+    }
+
+    listsSubjects() {
+        let phrase = this.favSubjects.reduce(function(total, current, index, source) {
+            return index === source.length - 1 ? total + ', and ' + current : total + ', ' + current;
+        });
+        phrase = `${this.name} likes ${phrase}.`;
+        console.log(phrase);
+    }
+
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge for ${subject}`);
+    }
+
+    graduate() {
+        if(this.grade >= 70) {
+            console.log(`${this.name} graduates from ${this.className}!`);
+        }
+        else {
+            console.log(`${this.name} isn't ready to graduate yet...`);
+        }
+    }
+}
+
+class Project_Manager extends Person {
+    constructor(person) {
+        super(person);
+        this.gradClassName = person.gradClassName;
+        this.favInstructor = person.favInstructor;
+
+    }
+
+    standUp(channel) {
+        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    }
+
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`);
+    }
+
+    modifyGrade(student) {
+        let modifyAmount = Math.floor(Math.random() * 100);
+        let possible = [1,2];
+        let operation = Math.floor(Math.random() * possible.length);
+        if(operation) student.grade += modifyAmount;
+        else student.grade -= modifyAmount;
+    }
+
+}
+
+const instructor = new Instructor({
+    name: 'Seijuro Hiko',
+    role: 'Master',
+    location: 'Japan',
+    age: 30,
+    favLanguage: 'Japanese',
+    specialty: 'Hiten Mitsurugi',
+    catchPhrase: 'It is foolish to listen to one who will not listen to you. Do as you wish.'
+});
+
+const student = new Student({
+    name: 'Kenshin Himura',
+    role: 'Battousai',
+    location: 'Japan',
+    age: 15,
+    previousBackground: 'Peasant',
+    className: 'Seijuro Hiko\'s Students',
+    favSubjects: ['Bringing Peace & Freedom', 'Saving Lives', 'Sleeping with his Sword']
+});
+
+const project_manager = new Project_Manager({
+    name: 'Katsura Kogoro',
+    role: 'Leader of Chosu Ishin Shishi',
+    location: 'Japan',
+    age: 31,
+    gradClassName: 'Chosu Ishin Shishi',
+    favInstructor: 'Seijuro Hiko'
+});
+
+console.log(instructor.name);
+console.log(instructor.age);
+console.log(instructor.catchPhrase);
+Instructor.demo('battojutsu');
+
+console.log(student.name);
+console.log(student.previousBackground);
+console.log(student.favSubjects);
+student.listsSubjects();
+student.PRAssignment('battojutsu');
+Instructor.grade(student, 'battojutsu');
+student.sprintChallenge('Hiten Mitsurugi');
+
+console.log(project_manager.name);
+console.log(project_manager.role);
+console.log(project_manager.gradClassName);
+project_manager.standUp('Kyoto');
+project_manager.debugsCode(student, 'Meiji Revolution');
+
+instructor.modifyGrade(student);
+console.log(student.grade);
+student.graduate();
+
+project_manager.modifyGrade(student);
+console.log(student.grade);
+student.graduate();
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -6,4 +6,224 @@ Prototype Refactor
 
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
+*//*
+  === GameObject ===
+  * createdAt
+  * name
+  * dimensions (These represent the character's size in the video game)
+  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+
+/*
+  === CharacterStats ===
+  * healthPoints
+  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+  * should inherit destroy() from GameObject's prototype
+*/
+
+/*
+  === Humanoid (Having an appearance or character resembling that of a human.) ===
+  * team
+  * weapons
+  * language
+  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+  * should inherit destroy() from GameObject through CharacterStats
+  * should inherit takeDamage() from CharacterStats
+*/
+
+/*
+  * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+  * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+  * Instances of CharacterStats should have all of the same properties as GameObject.
+*/
+
+// Test you work by un-commenting these 3 objects and the list of console logs below:
+
+class GameObject {
+    constructor(attributes) {
+        this.createdAt = attributes.createdAt;
+        this.name = attributes.name;
+        this.dimensions = attributes.dimensions;
+    }
+
+    destroy() {
+        return `${this.name} was removed from the game.`;
+    }
+}
+
+class CharacterStats extends GameObject {
+    constructor(attributes) {
+        super(attributes);
+        this.healthPoints = attributes.healthPoints;
+        this.attack = attributes.attack;
+    }
+
+    takeDamage(damage) {
+        this.healthPoints -= damage;
+        console.log(`${this.name} has ${this.healthPoints} health left.`);
+        if(this.healthPoints <= 0) {
+            console.log(this.destroy()); // this will display before the damage message
+        }
+        return `${this.name} took damage.`
+    }
+
+}
+
+class Humanoid extends CharacterStats {
+    constructor(attributes) {
+        super(attributes);
+        this.team = attributes.team;
+        this.weapons = attributes.weapons;
+        this.language = attributes.language;
+        this.hasHeroWeapon = attributes.hasHeroWeapon;
+    }
+
+    greet() {
+        return `${this.name} offers a greeting in ${this.language}.`;
+    }
+
+    destroy() {
+        return `${this.name} has been defeated!`;
+    }
+
+    Attack(target) {
+        if(typeof target) {
+            let damage = this.attack;
+            if(this.hasHeroWeapon === 1) damage += 10;
+            console.log(`${this.name} attacks ${target.name} with his ${this.weapons[0]}.`);
+            target.takeDamage(damage);
+        }
+    }
+}
+
+class Hero extends Humanoid {
+    constructor(attributes) {
+        super(attributes);
+    }
+
+}
+
+class Villain extends Humanoid {
+    constructor(attributes) {
+        super(attributes);
+    }
+
+}
+
+const mage = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+    },
+    healthPoints: 5,
+    attack: 8,
+    hasHeroWeapon: 0,
+    name: 'Bruce',
+    team: 'Mage Guild',
+    weapons: [
+        'Staff of Shamalama',
+    ],
+    language: 'Common Tongue',
+});
+
+const swordsman = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+    },
+    healthPoints: 15,
+    attack: 10,
+    hasHeroWeapon: 0,
+    name: 'Sir Mustachio',
+    team: 'The Round Table',
+    weapons: [
+        'Giant Sword',
+        'Shield',
+    ],
+    language: 'Common Tongue',
+});
+
+const archer = new Humanoid({
+    createdAt: new Date(),
+    dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+    },
+    healthPoints: 10,
+    attack: 8,
+    hasHeroWeapon: 0,
+    name: 'Lilith',
+    team: 'Forest Kingdom',
+    weapons: [
+        'Bow',
+        'Dagger',
+    ],
+    language: 'Elvish',
+});
+
+const swordHero = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+        length: 2,
+        width: 2,
+        height: 4,
+    },
+    healthPoints: 30,
+    attack: 10,
+    hasHeroWeapon: 1,
+    name: 'Link',
+    team: 'Kokiri Forest',
+    weapons: [
+        'Master Sword',
+        'Long Sword',
+    ],
+    language: 'Hyrulian',
+});
+
+const evilVillain = new Villain({
+    createdAt: new Date(),
+    dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+    },
+    healthPoints: 35,
+    attack: 12,
+    hasHeroWeapon: 0,
+    name: 'Ganondorf',
+    team: 'Gerudo Kingdom',
+    weapons: [
+        'Broad Claymore',
+        'Light Blast',
+    ],
+    language: 'Hyrulian',
+});
+
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+console.log(`The hero, ${swordHero.name}, enters the battlefield.`);
+console.log(`The villain, ${evilVillain.name}, stands there with an ominous grin.`);
+console.log(`${evilVillain.name} brandishes his ${evilVillain.weapons[0]}!`);
+console.log(`${swordHero.name} stands ready, the ${swordHero.weapons[0]} glows and gives him power!`);
+console.log(`The two clash, deciding the fate of the lands!!`);
+
+let counter = 0;
+while(swordHero.healthPoints > 0 && evilVillain.healthPoints > 0 && counter < 5) {
+    swordHero.Attack(evilVillain);
+    if(evilVillain.healthPoints > 0) evilVillain.Attack(swordHero);
+    counter++;
+}
